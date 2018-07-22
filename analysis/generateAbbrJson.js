620346118429db7e97ef047c4615f64bd80817b0
Steven Zhao <zhaoyu0810@gmail.com>
2018-07-21 23:24:17 -0700

    Output word frequencies

--- analysis/generateAbbrJson.js
+++ analysis/generateAbbrJson.js
@@ -1,31 +1,68 @@
 var fs = require('fs');
 
-
-function getWordCountsDic() {
-    var helpText = fs.readFileSync("../data/help_dump.json", 'utf8');
-    helpText = helpText
+function countWordsInStr(str, counts) {
+    str = str
+        .replace(/[:-_.\/]/g, ' ')
         .replace(/[^\w\s]/gi, '')
+        .replace(/[0-9]/g, '')
+        .replace(/\n/g, " ")
         .replace(/ +(?= )/g, '')
         .toLowerCase();
 
-    var lines = helpText.split("\n");
-
-    var tokens = helpText.split(" ");
-    var dic = {};
-    for (var i = 1, len = tokens.length; i < len; i++) {
+    var tokens = str.split(" ");
+    for (var i = 0, len = tokens.length; i < len; i++) {
         var word = tokens[i];
-        if (dic[word]) {
-            dic[word] = 0;
-        }
+        if (word && word.length > 1) {
+            if(word == "sourcerepositorysourcetag")
+                debugger;
+            if (!counts[word]) {
+                counts[word] = 0;
+            }
 
-        dic[word]++;
+            counts[word]++;
+        }
+    }
+}
+
+function count(obj, counts) {
+    if (!obj) {
+        return;
     }
 
-    return dic;
+    if (typeof obj === "string") {
+        countWordsInStr(obj, counts);
+        return;
+    }
+
+    if (Array.isArray(obj)) {
+        for (var i = 1, len = obj.length; i < len; i++) {
+            count(obj[key], counts);
+        }
+    }
+
+    for (var key in obj) {
+        // if (key === "parameters") {
+        //     continue;
+        // }
+
+        count(obj[key], counts);
+    }
+}
+
+function getWordCountsDic() {
+    var helpText = fs.readFileSync("../data/help_dump.json", 'utf8');
+    // have to parse to obj to count the workd, since the helpText is too large and below code hang 
+    //  var lines = helpText.split("\n");
+    var root = JSON.parse(helpText);
+    var counts = {};
+    count(root, counts)
+    var r = JSON.stringify(counts);
+    fs.writeFileSync("../data/help_dump-word-frequencies.json", JSON.stringify(counts, null, 4));
+    return counts;
 }
 
 function run() {
-    // var wordCounts = getWordCountsDic(); // TODO
+    var wordCounts = getWordCountsDic(); // TODO
     var text = fs.readFileSync('abbr.csv', 'utf8');
     var lines = text.split("\n");
 
@@ -35,13 +72,13 @@ function run() {
         var tokens = line.split(',');
         var abbr = tokens[0];
         var word = tokens[1];
-        var count = parseInt(tokens[7]);
         if (word && abbr) {
-            result[abbr.toLowerCase()] = { word: word.toLowerCase(), count: count };
+            result[abbr.toLowerCase()] = { word: word.toLowerCase(), count: wordCounts[abbr] };
         }
     }
 
     fs.writeFileSync("../data/abbr.json", JSON.stringify(result, 0, 4));
+    console.log("completed");
 }
 
 run();
