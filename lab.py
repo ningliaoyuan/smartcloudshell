cc82463eba51f417c35bc2f0f8740ae54ba49003
Lynn Ning <lynning@microsoft.com>
2018-07-23 15:54:04 -0700

    added loadFromYamlFile functions to TestReport and TestReportDiff

--- lab.py
+++ lab.py
@@ -6,7 +6,7 @@ import modelFactory
 # from Model import baselineModel_lg, baselineModel_sm
 from modelBase import CliNlpModel, Suggestion
 from measure.testset import TestCase, TestSet, testset_queries
-from measure.testrunner import TestRunner, TestReportDiff
+from measure.testrunner import TestRunner, TestReportDiff, TestReport
 
 def ensureTestRunerCanRun():
   runner = TestRunner(testset_queries, modelFactory.getBaselineModel_sm())
@@ -16,9 +16,11 @@ def ensureTestRunerCanRun():
 def compare(testSet: TestSet, model1: CliNlpModel, model2: CliNlpModel):
   runner = TestRunner(testSet, model1)
   report1 = runner.run()
+  report1.saveToYamlFile()
 
   runner = TestRunner(testSet, model2)
   report2 = runner.run()
+  report2.saveToYamlFile()
 
   diff = TestReportDiff.diffReports(report1, report2)
   diff.saveToYamlFile()
@@ -29,7 +31,14 @@ def compareSmVsLgModels():
 def compareAbbrModelVsBaseLine():
   compare(testset_queries, modelFactory.getModelWithAbbrVectorAssigned(), modelFactory.getBaselineModel())
 
-ensureTestRunerCanRun()
+def compareReports(reportPath1: str, reportPath2: str):
+  report1 = TestReport.loadFromYamlFile(reportPath1)
+  report2 = TestReport.loadFromYamlFile(reportPath2)
+  diff = TestReportDiff.diffReports(report1, report2)
+  diff.saveToYamlFile()
+
+# ensureTestRunerCanRun()
 # compareAbbrModelVsBaseLine()
 
+compareReports('0723113453_queries_lgd_lgm.report', '0723113442_queries_lgd_lgm_abbr.report')
 # TODO: add more runner to measure different combinations
